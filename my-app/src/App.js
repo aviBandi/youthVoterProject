20972e44ec56d89d033bd4ddc53dbcd17f5bcb6d
Avi Bandi <bandiavi000@isd284.com>
2024-02-24 15:59:35 -0600

    updated nav code
    no physicla change, but separated the nav bar from the hero section into it's own component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import Hero from './sections/hero';
+import Nav from './components/nav';
 function App() {
   return (
     <div className="App">
+        < Nav />
       <section>
+
         < Hero />
       </section>
     </div>
