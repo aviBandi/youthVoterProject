0c706a152c3371071bec8990fbe301b6b0650a00
Avi Bandi <bandiavi000@isd284.com>
2024-02-24 18:16:18 -0600

    ok added secratary of state link on hero

--- my-app/src/sections/hero.js
+++ my-app/src/sections/hero.js
@@ -1,7 +1,6 @@
 import bg from "../images/heroPic.png"
 
 
-
 export default function Hero() {
 
     const style = {
@@ -43,7 +42,8 @@ export default function Hero() {
                         </p>
                         <div className="mt-10 flex items-center justify-center gap-x-6">
                             <a
-                                href="#"
+                                href="https://mnvotes.sos.mn.gov/VoterRegistration/index"
+                                target="_blank"
                                 className="rounded-md bg-blue-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-blue-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                             >
                                 Register to Vote <span aria-hidden="true">→</span>
