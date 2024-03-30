7c6bff53b35d81b42fb75694e5aec5dfead8a719
Avi Bandi <bandiavi000@isd284.com>
2024-03-30 16:36:26 -0500

    made some changes

--- my-app/src/pages/home.js
+++ my-app/src/pages/home.js
@@ -4,12 +4,16 @@ import Mission from "../sections/mission"
 import ResourcesHome from "../sections/resourcesHome"
 import Impacts from "../sections/impacts"
 import GetStarted from "../sections/getStarted"
+import Stats from "../sections/stats"
+import TrustedBy from "../sections/trustedby"
 const home = () => {
     return (
         <div className="">
             <Nav />
             <Hero />
+            <TrustedBy />
             <Mission />
+            <Stats />
             <ResourcesHome />
             <Impacts />
             <GetStarted />
