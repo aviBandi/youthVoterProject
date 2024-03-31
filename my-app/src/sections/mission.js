57a3e6fc5dea391005c83396135b39f8e8ac7a5e
Avi Bandi <bandiavi000@isd284.com>
2024-03-31 00:48:25 -0500

    made the trustedBy and mission look a bit better

--- my-app/src/sections/mission.js
+++ my-app/src/sections/mission.js
@@ -1,4 +1,4 @@
-import { CloudArrowUpIcon, LockClosedIcon, ServerIcon } from '@heroicons/react/20/solid'
+import { CheckIcon, CloudArrowUpIcon, DocumentCheckIcon, LockClosedIcon, ServerIcon } from '@heroicons/react/20/solid'
 
 const features = [
 
@@ -7,29 +7,24 @@ const features = [
     name: 'Push to deploy.',
     description:
       'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
-    icon: CloudArrowUpIcon,
+    icon: CheckIcon,
   },
   {
     name: 'SSL certificates.',
     description: 'Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.',
-    icon: LockClosedIcon,
+    icon: DocumentCheckIcon,
   },
-  {
-    name: 'Database backups.',
-    description: 'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.',
-    icon: ServerIcon,
-  },
+
 ]
 
 export default function Example() {
   return (
-    <div className="overflow-hidden bg-white py-24 sm:py-32">
+    <div className="overflow-hidden bg-white pt-24 sm:pt-16">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
           <div className="lg:pr-8 lg:pt-4">
             <div className="lg:max-w-lg">
-              <h2 className="text-base font-semibold leading-7 text-indigo-600">About Us!</h2>
-              <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">A better workflow</p>
+              <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">About The Youth Voter Project!</p>
               <p className="mt-6 text-lg leading-8 text-gray-600">
                 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque,
                 iste dolor cupiditate blanditiis ratione.
@@ -45,12 +40,41 @@ export default function Example() {
                   </div>
                 ))}
               </dl>
+              <div>
+
+                <dl className="grid grid-cols-1 pt-7 lg:grid-cols-3">
+                    <div className="mx-auto flex max-w-xs flex-col gap-y-4">
+                      <h2 className="text-base leading-7 text-gray-600">Schools Connected</h2>
+                      <h1 className="order-first text-3xl font-semibold tracking-tight text-gray-900 sm:text-3xl">
+                        13,000+
+                      </h1>
+                    </div>
+
+                    <div className="mx-auto flex max-w-xs flex-col gap-y-4">
+                      <h2 className="text-base leading-7 text-gray-600">Schools Connected</h2>
+                      <h1 className="order-first text-3xl font-semibold tracking-tight text-gray-900 sm:text-3xl">
+                        13,000+
+                      </h1>
+                    </div>
+
+                    <div className="mx-auto flex max-w-xs flex-col gap-y-4">
+                      <h2 className="text-base leading-7 text-gray-600">Schools Connected</h2>
+                      <h1 className="order-first text-3xl font-semibold tracking-tight text-gray-900 sm:text-3xl">
+                        13,000+
+                      </h1>
+                    </div>
+
+
+                </dl>
+
+
+              </div>
             </div>
           </div>
           <img
-            src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png"
+            src="https://media.11alive.com/assets/WXIA/images/c64e2bd0-78d5-4fb8-b625-4ad37e473165/c64e2bd0-78d5-4fb8-b625-4ad37e473165_1920x1080.jpg"
             alt="Product screenshot"
-            className="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
+            className="w-[48rem] max-w-none rounded-xl border-black border-1 shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
             width={2432}
             height={1442}
           />
