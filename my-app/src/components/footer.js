6c684cddee55dbe801e71d0c9525e029dfa1a6b3
Avi Bandi <bandiavi000@isd284.com>
2024-04-02 01:59:48 -0500

    lowk this website making me sleepy
    gn

--- my-app/src/components/footer.js
+++ my-app/src/components/footer.js
@@ -19,15 +19,15 @@ export default function Footer() {
 
           <nav>
             <h6 className="footer-title text-white">Pages</h6>
-            <a className="link link-hover text-white">Home</a>
-            <a className="link link-hover text-white">Resources</a>
-            <a className="link link-hover text-white">Team</a>
-            <a className="link link-hover text-white">Contact</a>
+            <a href='/home' className="link link-hover text-white">Home</a>
+            <a href='/resources' className="link link-hover text-white">Resources</a>
+            <a href='/team' className="link link-hover text-white">Team</a>
+            <a href='/contact' className="link link-hover text-white">Contact</a>
           </nav>
           <nav>
             <h6 className="footer-title text-white">Socials</h6>
-            <a className="link link-hover text-white">Instagram</a>
-            <a className="link link-hover text-white">Linkedin</a>
+            <a target="_blank" className="link link-hover text-white">Instagram</a>
+            <a href="https://www.linkedin.com/company/youthvoterproject/" target='_blank' className="link link-hover text-white">Linkedin</a>
           </nav>
           <nav>
             <h6 className="footer-title text-white">Legal</h6>
