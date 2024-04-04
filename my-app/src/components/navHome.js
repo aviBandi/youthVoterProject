f0faa6d34372834f8f92158ca0d3bef9a3d18ff2
aviBandi <bandiavi000@isd284.com>
2024-04-03 19:46:01 -0500

    changed color of the three  bar mobile nav on mobile view
    wow chatgpt came in clutch

--- my-app/src/components/navHome.js
+++ my-app/src/components/navHome.js
@@ -41,7 +41,7 @@ const Nav = () => {
                         >
                             <span className="sr-only">Open main menu</span>
 
-                            <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="#ef4444" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                            <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" strokeWidth={1.5} stroke="white" className="w-6 h-6">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                             </svg>
 
