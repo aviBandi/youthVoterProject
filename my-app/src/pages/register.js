767720c47d75ad58d0924d4a67e989882751dc74
aviBandi <140932033+aviBandi@users.noreply.github.com>
2024-07-14 23:44:38 -0500

    changed up kok a bit

--- my-app/src/pages/register.js
+++ my-app/src/pages/register.js
@@ -32,10 +32,10 @@ const Register = () => {
             <NavHome />
 
             <div className='flex items-center justify-center h-screen'>
-                <button onClick={incrementNumber}>
+                <button onClick={incrementNumber} >
                     <a href="https://mnvotes.sos.mn.gov/VoterRegistration/index" target="_blank">
                         <div className="flex items-center justify-center gap-x-6">
-                            <div className="rounded-md border-2 border-blue-600 bg-blue-600 px-3.5 py-10 text-3xl md:text-7xl font-semibold text-white shadow-sm hover:bg-white hover:text-blue-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 transition-all">
+                            <div className="rounded-xl border-2 border-red-500 bg-blue-600 px-3.5 py-10 text-3xl md:text-7xl font-semibold text-white shadow-sm hover:bg-white hover:text-blue-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 transition-all">
                                 Register to Vote <span aria-hidden="true">→</span>
                             </div>
                         </div>
