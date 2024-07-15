3017a85c2b00b56ee92cc69f0aea8880060f3c73
aviBandi <140932033+aviBandi@users.noreply.github.com>
2024-07-15 17:24:43 -0500

    got it to work for the firebases for each invidual day as well

--- my-app/src/pages/register.js
+++ my-app/src/pages/register.js
@@ -9,18 +9,51 @@ import "../firebase";
 
 const Register = () => {
 
+    const getCurrentDate = () => {
+        const date = new Date();
+        const year = date.getFullYear();
+        const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are 0-indexed
+        const day = String(date.getDate()).padStart(2, '0');
+
+        return `${year}-${month}-${day}`;
+    };
+
     const incrementNumber = async () => {
 
 
         try {
-            const docRef = doc(db, 'clicks', 'clicks');
-            const value = await getDoc(docRef);
-            const totalClicks = value.data();
-            console.log(totalClicks.totalClicks);
-            await updateDoc(docRef, {
-                totalClicks: totalClicks.totalClicks + 1
+            // const docTotal = doc(db, 'clicks', 'totalClicks');
+            // const value = await getDoc(docTotal);
+            // const totalClicks = value.data();
+            // console.log(getCurrentDate());
+            // await updateDoc(docTotal, {
+            //     totalClicks: totalClicks.totalClicks + 1
+            // });
+            // console.log('Document successfully updated!');
+
+
+            const docTotal = doc(db, 'clicks', 'totalClicks');
+            const docRef = doc(db, 'clicks', getCurrentDate());
+
+            const docSnapDay = await getDoc(docRef);
+            const docSnapTotal = await getDoc(docTotal);
+
+            if (docSnapDay.exists()) {
+                const data = docSnapDay.data();
+                await updateDoc(docRef, {
+                    totalClicks: data.totalClicks + 1
+                });
+            } else {
+                await setDoc(docRef, {
+                    totalClicks: 1
+                });
+            }
+
+            await updateDoc(docTotal, {
+                totalClicks: docSnapTotal.data().totalClicks + 1
             });
-            console.log('Document successfully updated!');
+
+
         } catch (error) {
             console.error('Error updating document: ', error);
         }
