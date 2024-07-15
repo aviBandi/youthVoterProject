7d05645b934aad3519c823e0e10a48dfcc457230
aviBandi <140932033+aviBandi@users.noreply.github.com>
2024-07-14 23:38:33 -0500

    should be a bettter looking bit

--- my-app/src/pages/register.js
+++ my-app/src/pages/register.js
@@ -1,23 +1,14 @@
 import React from 'react';
 
 import { db, doc, setDoc, firebase } from '../firebase';
-import { FieldValue, getDoc, updateDoc } from 'firebase/firestore';
+import { getDoc, updateDoc } from 'firebase/firestore';
+
+import NavHome from '../components/navHome';
+
 import "../firebase";
 
 const Register = () => {
 
-    // const incrementNumber = async () => {
-
-    //     const docRef = doc(db, 'clicks', 'clicks');
-
-    //     try {
-    //         await updateDoc(docRef, {
-    //             totalClicks: FieldValue.increment(1)
-    //         });
-    //     } catch (e) {
-    //         console.error(e);
-    //     }
-    // };
     const incrementNumber = async () => {
 
 
@@ -37,16 +28,20 @@ const Register = () => {
 
 
     return (
-        <div className='flex items-center justify-center h-screen'>
-            <button onClick={incrementNumber}>
-                <a href="https://mnvotes.sos.mn.gov/VoterRegistration/index" target="_blank">
-                    <div className="flex items-center justify-center gap-x-6">
-                        <div className="rounded-md border-2 border-blue-600 bg-blue-600 px-3.5 py-10 text-3xl md:text-7xl font-semibold text-white shadow-sm hover:bg-white hover:text-blue-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 transition-all">
-                            Register to Vote <span aria-hidden="true">→</span>
+        <div className='overflow-auto h-full bg-gray-800'>
+            <NavHome />
+
+            <div className='flex items-center justify-center h-screen'>
+                <button onClick={incrementNumber}>
+                    <a href="https://mnvotes.sos.mn.gov/VoterRegistration/index" target="_blank">
+                        <div className="flex items-center justify-center gap-x-6">
+                            <div className="rounded-md border-2 border-blue-600 bg-blue-600 px-3.5 py-10 text-3xl md:text-7xl font-semibold text-white shadow-sm hover:bg-white hover:text-blue-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 transition-all">
+                                Register to Vote <span aria-hidden="true">→</span>
+                            </div>
                         </div>
-                    </div>
-                </a>
-            </button>
+                    </a>
+                </button>
+            </div>
         </div>
     )
 }
