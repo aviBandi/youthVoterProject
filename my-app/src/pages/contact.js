dfdadddec5c60fc6fa74cd128b5bc82048961734
Avi Bandi <bandiavi000@isd284.com>
2024-08-05 21:59:38 -0500

    added contact

--- my-app/src/pages/contact.js
+++ my-app/src/pages/contact.js
@@ -74,6 +74,21 @@ export default function Contact() {
           </div>
 
           <div className="sm:col-span-2">
+            <label htmlFor="Phone" className="block text-sm font-semibold leading-6 text-gray-900">
+              Phone Number
+            </label>
+            <div className="mt-2.5">
+              <input
+                type="phone"
+                name="Phone Number"
+                id="Phone"
+                
+                className="bg-white block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-500 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+
+          <div className="sm:col-span-2">
             <label htmlFor="email" className="block text-sm font-semibold leading-6 text-gray-900">
               Email
             </label>
